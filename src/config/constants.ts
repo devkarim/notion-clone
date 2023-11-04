44646fc55c071e948273c49dcc6d0076a21cfd7d
Karim <devkarim@hotmail.com>
2023-11-04 22:19:31 +0200

    feat: add app constants

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const __dev__ = process.env.NODE_ENV === 'development';
+export const __prod__ = process.env.NODE_ENV === 'production';
+
+export const APP_NAME = 'Motion';
+export const APP_VERSION = '1.0.0';
+export const APP_URL = 'https://motion.karimwael.com';
