cd3a9828a2a09ca00ba6baa3dc124cf29d1eb322
Karim <devkarim@hotmail.com>
2023-11-09 03:42:11 +0200

    feat: display navbar and footer in guest layout

--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -1,7 +1,18 @@
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="h-full">
+      <Navbar />
+      <main className="h-full pt-28">
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
 }
