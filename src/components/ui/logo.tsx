6696ae03b3a10adaad5be6551957dcb9cf53255e
Karim <devkarim@hotmail.com>
2024-01-24 18:09:39 +0200

    refactor: allow more params for logo
    Allow passing more parameters to logo component by extending HTML span props.

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,13 +1,11 @@
 import { cn } from '@/lib/utils';
 import { APP_NAME } from '@/config/constants';
 
-interface LogoProps {
-  className?: string;
-}
+interface LogoProps extends React.HTMLProps<HTMLSpanElement> {}
 
-const Logo: React.FC<LogoProps> = ({ className }) => {
+const Logo: React.FC<LogoProps> = ({ className, ...props }) => {
   return (
-    <span className={cn('font-bold text-3xl font-serif', className)}>
+    <span className={cn('font-bold text-3xl font-serif', className)} {...props}>
       {APP_NAME}
     </span>
   );
