c187df2891af4a591dc3eb4cb9f0eb1638199c0a
Karim <devkarim@hotmail.com>
2024-01-24 18:13:02 +0200

    feat: display guest home page components

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,3 +1,17 @@
+import Container from '@/components/ui/container';
+
+import Header from './components/header';
+import Heroes from './components/heroes';
+import Subtitle from './components/subtitle';
+import GetStartedButton from './components/get-started-button';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Container className="flex min-h-full flex-col gap-8 items-center w-full">
+      <Header />
+      <Subtitle />
+      <GetStartedButton size="default" showArrow />
+      <Heroes />
+    </Container>
+  );
 }
