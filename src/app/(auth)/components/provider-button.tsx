5ecfa26b89f5ab62e0be4a4b130b1257d63fbdd8
Karim <devkarim@hotmail.com>
2024-01-29 01:21:07 +0200

    feat: add onClick arg to provider-button

--- src/app/(auth)/components/provider-button.tsx
+++ src/app/(auth)/components/provider-button.tsx
@@ -1,13 +1,20 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 
 interface ProviderButtonProps {
   name: string;
   icon: React.ReactNode;
+  onClick: () => void;
 }
 
-const ProviderButton: React.FC<ProviderButtonProps> = ({ name, icon }) => {
+const ProviderButton: React.FC<ProviderButtonProps> = ({
+  name,
+  icon,
+  onClick,
+}) => {
   return (
-    <Button variant="classic" className="flex w-80">
+    <Button variant="classic" className="flex w-80" onClick={onClick}>
       <span className="mr-2 text-xl">{icon}</span>
       <p>Continue with {name}</p>
     </Button>
