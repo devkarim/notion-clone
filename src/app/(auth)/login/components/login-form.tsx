8708f5f03ceca6d38b9d51380002f3cb0b193ef4
Karim <devkarim@hotmail.com>
2024-01-29 01:27:18 +0200

    feat: add login with email functionality

--- src/app/(auth)/login/components/login-form.tsx
+++ src/app/(auth)/login/components/login-form.tsx
@@ -1,17 +1,32 @@
 'use client';
 
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { createBrowserClient } from '@supabase/ssr';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { AuthSchema, authSchema } from '@/schemas/auth-schema';
-import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
 interface LoginFormProps {}
 
 const LoginForm: React.FC<LoginFormProps> = ({}) => {
+  const router = useRouter();
+  const [err, setError] = useState('');
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
   const form = useForm<AuthSchema>({
     resolver: zodResolver(authSchema),
     defaultValues: {
@@ -20,17 +35,28 @@ const LoginForm: React.FC<LoginFormProps> = ({}) => {
     },
   });
 
-  const onSubmit = (data: AuthSchema) => {};
+  const onSubmit = async ({ email, password }: AuthSchema) => {
+    setError('');
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) return setError(error.message);
+    router.push('/dashboard');
+  };
+
+  const isSubmitting = form.formState.isSubmitting;
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
         <FormField
           control={form.control}
           name="email"
+          disabled={isSubmitting}
           render={({ field }) => (
             <FormItem>
-              <Label>Email</Label>
+              <FormLabel>Email</FormLabel>
               <FormControl>
                 <Input
                   type="email"
@@ -38,17 +64,32 @@ const LoginForm: React.FC<LoginFormProps> = ({}) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" className="w-full">
+        <FormField
+          control={form.control}
+          name="password"
+          disabled={isSubmitting}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter your password..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full" disabled={isSubmitting}>
           Continue with email
         </Button>
-        {form.formState.errors.email && (
-          <p className="text-sm text-center text-destructive">
-            {form.formState.errors.email.message}
-          </p>
-        )}
+        {err && <p className="text-center text-destructive text-sm">{err}</p>}
       </form>
     </Form>
   );
