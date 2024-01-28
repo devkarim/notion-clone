fbb0f33267a7a3b80da7842be1bbaa9afd3551d1
Karim <devkarim@hotmail.com>
2024-01-28 02:29:34 +0200

    fix: change APP_URL when on dev mode

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,6 @@ export const __prod__ = process.env.NODE_ENV === 'production';
 
 export const APP_NAME = 'Motion';
 export const APP_VERSION = '1.0.0';
-export const APP_URL = 'https://motion.karimwael.com';
+export const APP_URL = __dev__
+  ? 'http://localhost:3000'
+  : 'https://motion.karimwael.com';
