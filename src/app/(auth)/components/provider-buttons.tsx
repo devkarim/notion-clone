b22f9b722f5e51a1be97f6063e442d240fcdf4e2
Karim <devkarim@hotmail.com>
2024-01-29 01:27:05 +0200

    feat: add login with oauth functionality

--- src/app/(auth)/components/provider-buttons.tsx
+++ src/app/(auth)/components/provider-buttons.tsx
@@ -1,17 +1,42 @@
 'use client';
 
+import toast from 'react-hot-toast';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
+import { createBrowserClient } from '@supabase/ssr';
+
+import { APP_URL } from '@/config/constants';
 
 import ProviderButton from './provider-button';
 
 interface ProviderButtonsProps {}
 
 const ProviderButtons: React.FC<ProviderButtonsProps> = ({}) => {
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+
+  const onContinueWith = async (provider: 'google' | 'github') => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: { redirectTo: `${APP_URL}/api/auth/callback` },
+    });
+    if (error) return toast.error(error.message);
+  };
+
   return (
     <div className="space-y-3">
-      <ProviderButton name="Google" icon={<FcGoogle />} />
-      <ProviderButton name="GitHub" icon={<FaGithub />} />
+      <ProviderButton
+        name="Google"
+        icon={<FcGoogle />}
+        onClick={() => onContinueWith('google')}
+      />
+      <ProviderButton
+        name="GitHub"
+        icon={<FaGithub />}
+        onClick={() => onContinueWith('github')}
+      />
     </div>
   );
 };
